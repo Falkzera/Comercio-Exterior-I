c2b3cf389df0adb529316d9178291c24a26027a2
Falkzera <falcovisk@gmail.com>
2025-03-31 19:23:16 -0300

    Fix

--- automacao_coleta/utils/datas.py
+++ automacao_coleta/utils/datas.py
@@ -4,5 +4,5 @@ import streamlit as st
 def get_ano_atual():
     return datetime.now().year
 
-def get_range_historico(ano_inicio=2023):
+def get_range_historico(ano_inicio=2025):
     return list(range(ano_inicio, get_ano_atual() + 1))  # até ano atual - 2
